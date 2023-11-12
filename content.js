c92028ba274113edaaf95eaad4856e03702fd826
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-11-12 14:48:02 -0800

    Paste Hotfix

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
 document.addEventListener('paste', (event) => {
     let clipboardData = event.clipboardData || window.clipboardData;
+    let activeElement = document.activeElement;
 
-    if (clipboardData.items) {
-        for (let i = 0; i < clipboardData.items.length; i++) {
-            if (clipboardData.items[i].kind === 'file') {
-                let file = clipboardData.items[i].getAsFile();
+    if (activeElement.tagName === 'INPUT' && activeElement.type === 'file') {
+        if (clipboardData.items) {
+            for (let i = 0; i < clipboardData.items.length; i++) {
+                if (clipboardData.items[i].kind === 'file') {
+                    let file = clipboardData.items[i].getAsFile();
 
-                let activeElement = document.activeElement;
-                if (activeElement.tagName === 'INPUT' && activeElement.type === 'file') {
                     let dataTransfer = new DataTransfer();
                     dataTransfer.items.add(file);
 
                     activeElement.files = dataTransfer.files;
+
                     event.preventDefault();
                 }
             }
